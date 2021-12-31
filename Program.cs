97fe603b5800af71a2c228454b22e351209b7731
Alex_Sour <52638772+Alex-Sour@users.noreply.github.com>
2021-12-31 15:49:15 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 using Human;
 using UnityEngine;
+using Human.Eyes;
 
 public class Alex_Sour : MonoBehaviour
 {
     public UnityGame pylonPixel;
+    public UnityGame pylonPixel2;
+    
     void Update()
     {
-        WorkOnUnityGame(pylonPixel);
+        if(Random.Range(0, 2) == 0)
+        {
+            WorkOnUnityGame(pylonPixel2);
+        }
+        else
+        {
+            Eyes.WatchWebURL("https://youtube.com/");
+        }
     }
 }
