406159051adbecee9a9c867f1ef52db22dc9e494
Alex_Sour <52638772+Alex-Sour@users.noreply.github.com>
2021-12-31 16:00:41 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,56 @@
 using Human;
 using UnityEngine;
 using Human.Eyes;
+using Human.Memory;
 
 public class Alex_Sour : MonoBehaviour
 {
+    public ShortTermMemory shortTermMemory;
+    public LongTermMemory longTermMemory;
     public UnityGame pylonPixel;
     public UnityGame pylonPixel2;
-    
+    public Eye[] eyes;
+    private bool isSleeping = false;
+    public GameObject monitor;
+    public ChromiumWebBrowser browser;
+
     void Update()
     {
-        if(Random.Range(0, 2) == 0)
+        TimeSpan start = new TimeSpan(10, 0, 0); //10 AM
+        TimeSpan end = new TimeSpan(23, 0, 0); // 11 PM
+        TimeSpan now = DateTime.Now.TimeOfDay;
+
+        isSleeping = (now > start) && (now < end); // If the current time is between 10 AM and 11 PM, I am awake.
+
+        foreach (Eye eye in eyes)
         {
-            WorkOnUnityGame(pylonPixel2);
+            eye.isOpen = !isSleeping;
+        }
+
+        if (!isSleeping)
+        {
+            if (Random.Range(0, 2) == 0) // 50% chance of working on Pylon Pixel 2
+            {
+                WorkOnUnityGame(pylonPixel2);
+            }
+            else // 50% chance of watching YouTube
+            {
+                browser.webURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+                foreach (Eye eye in eyes)
+                {
+                    eye.LookAt(monitor.transform);
+                }
+            }
         }
         else
         {
-            Eyes.WatchWebURL("https://youtube.com/");
+            Dream dream = new Dream();
+
+            if (Random.Range(0, 50) == 0) // I almost always forget my dreams :(
+            {
+                shortTermMemory.Add(dream);
+            }
         }
     }
 }
